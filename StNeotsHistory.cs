0c9716ae0280389f95e93ea36c639b804f758bca
Kieran Emery <kizwiz@hotmail.co.uk>
2023-02-10 11:32:31 +0000

    Adds a History class

---
+++ StNeotsHistory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StNeotsApp
+{
+    /// <summary>
+    /// History class
+    /// </summary>
+    public class StNeotsHistory
+    {
+        public string[] GetImportantEvents()
+        {
+            return new[]
+            {
+                "St. Neots was founded by Saint Neot in the 9th century.",
+                "In the 11th century, a priory was established in St. Neots.",
+                "St. Neots became a market town in the 13th century.",
+                "In the 19th century, St. Neots expanded due to the railway.",
+                "St. Neots became a town in 1951."
+            };
+        }
+    }
+}
